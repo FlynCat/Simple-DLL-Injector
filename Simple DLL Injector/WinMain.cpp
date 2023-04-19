9799617b7de8d43b688520f67b67b16a33c4c4f1
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-04-19 14:09:21 +0800

    fix warning

--- Simple DLL Injector/WinMain.cpp
+++ Simple DLL Injector/WinMain.cpp
@@ -1,9 +1,9 @@
 #include <Windows.h>
 int WINAPI WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nShowCmd
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
 ) {
 	return 0;
 }
