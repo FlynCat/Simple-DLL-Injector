aa8d15460c6845492daace25f98ff9a9a7ba1509
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-04-27 16:57:30 +0800

    use util::GetTime instead of imgui's

--- Simple DLL Injector/logger.h
+++ Simple DLL Injector/logger.h
@@ -11,7 +11,7 @@ namespace logger
     void    Draw(const char* title, bool* p_open = 0);
 };
 
-#define LOG_INFO(fmt, ...) logger::AddLog("[%.1f] " fmt "\n",ImGui::GetTime(), ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) logger::AddLog("[%s] " fmt "\n",util::GetTime().c_str(), ##__VA_ARGS__)
 #define LOG_DEBUG(fmt, ...) logger::AddLog("[DEBUG] " fmt "\n", ##__VA_ARGS__)
 #define LOG_ERROR(fmt, ...) logger::AddLog("[ERROR] " fmt "\n", ##__VA_ARGS__)
 
