617c7c420180f82775af336a18ef160267aa095b
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-04-28 11:50:03 +0800

    formatting

--- Simple DLL Injector/DirectX.h
+++ Simple DLL Injector/DirectX.h
@@ -2,13 +2,13 @@
 #include <Windows.h>
 namespace DirectX
 {
-	bool Init(HWND hWnd);
-	void Destroy();
-	void ImGuiInit();
-	void ImGuiDestroy();
-	void HandleResize(UINT newWidth, UINT newHeight);
-	void Begin();
-	void Render();
-	void End();
+    bool Init(HWND hWnd);
+    void Destroy();
+    void ImGuiInit();
+    void ImGuiDestroy();
+    void HandleResize(UINT newWidth, UINT newHeight);
+    void Begin();
+    void Render();
+    void End();
 };
 
