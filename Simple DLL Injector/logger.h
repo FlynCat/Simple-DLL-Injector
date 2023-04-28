1601f62ab97918b191102a65691a8d94d88c9573
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-04-28 13:09:26 +0800

    strip LOG_DEBUG in release mode

--- Simple DLL Injector/logger.h
+++ Simple DLL Injector/logger.h
@@ -12,6 +12,10 @@ namespace logger
 };
 
 #define LOG_INFO(fmt, ...) logger::AddLog("[%s] " fmt "\n",util::GetTime().c_str(), ##__VA_ARGS__)
+#ifdef _DEBUG
 #define LOG_DEBUG(fmt, ...) logger::AddLog("[DEBUG] " fmt "\n", ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(fmt, ...) 
+#endif
 #define LOG_ERROR(fmt, ...) logger::AddLog("[ERROR] " fmt "\n", ##__VA_ARGS__)
 
